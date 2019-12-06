57c401075deb43cee91d85c1d69bcc4c83e51713
Rafael Said <rafaelsaidbc@yahoo.com.br>
2019-12-06 11:20:29 -0300

    feat: add input robot and state robot

---
+++ index.js
@@ -0,0 +1,16 @@
+const readline = require('readline-sync')
+const robots = {
+  input: require('./robots/input.js'),
+  text: require('./robots/text.js'),
+  state: require('./robots/state.js')
+}
+
+async function start() {
+  robots.input()
+  await robots.text()
+
+  const content = robots.state.load()
+  console.dir(content, { depth: null } )
+}
+
+start()
